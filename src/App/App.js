4011b19a84a7c54e027ff2a1e0da1fe1616059f3
Saad Baradan <saadbaradan@gmail.com>
2019-12-11 17:22:22 -0700

    Add Header component

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "../Components/Header/Header";
 import "./App.css";
 
 export class App extends Component {
@@ -8,7 +9,12 @@ export class App extends Component {
   }
 
   render() {
-    return <div className="App">hi</div>;
+    return (
+      <div>
+        <Header />
+        <div className="App">hi</div>
+      </div>
+    );
   }
 }
 
