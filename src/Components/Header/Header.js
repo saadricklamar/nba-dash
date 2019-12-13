492548d1b7d0cdfc6276f54b7cfe6c0760ffe523
Saad Baradan <saadbaradan@gmail.com>
2019-12-13 15:16:18 -0700

    Add basic header with search bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div className="Header">I'm a header</div>;
+  return (
+    <div className="Header">
+      <header className="searchbar">
+        <button>NBA</button>
+        <input type="text" name="search" placeholder=" Search any player" />
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
